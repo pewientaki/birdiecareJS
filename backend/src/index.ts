bad37fe17f42361ee0861b7efb2ff594cfa61716
Krystian Stec <krystianstec0@gmail.com>
2020-08-10 01:32:53 +0100

    pre-deploy changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,6 @@
 import app from './application';
-const express = require('express');
-const path = require('path');
 
-const ENV = process.env.NODE_ENV;
 const port = process.env.PORT || 8000;
-
-if (ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  app.use((req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-}
 
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
