360a2cf57bbd525affa46973010063e95b6c2e13
Krystian Stec <krystianstec0@gmail.com>
2020-08-10 02:57:27 +0100

    routes updated

--- front-end2/src/store/saga/sagas.js
+++ front-end2/src/store/saga/sagas.js
@@ -3,7 +3,7 @@ import { call, put, takeEvery, all } from 'redux-saga/effects';
 
 export function* fetchNutritionData(action) {
     try {
-        const nutritionResponse = yield call(axios.get, 'http://localhost:8000/nutrition/' + action.payload.patientId);
+        const nutritionResponse = yield call(axios.get, 'https://birdiecare-api.herokuapp.com/nutrition/' + action.payload.patientId);
         yield put({ type: "DATA_NUTRITION_SUCCEEDED", nutrition: nutritionResponse.data.data });
     } catch (error) {
         yield put({ type: "DATA_WEEKLY_FAILED", message: error.message });
@@ -12,7 +12,7 @@ export function* fetchNutritionData(action) {
 
 export function* fetchHealthData(action) {
     try {
-        const healthResponse = yield call(axios.get, 'http://localhost:8000/health/' + action.payload.patientId);
+        const healthResponse = yield call(axios.get, 'https://birdiecare-api.herokuapp.com/health/' + action.payload.patientId);
         yield put({ type: "DATA_HEALTH_SUCCEEDED", health: healthResponse.data.data });
     } catch (error) {
         yield put({ type: "DATA_HEALTH_FAILED", message: error.message });
@@ -21,7 +21,7 @@ export function* fetchHealthData(action) {
 
 export function* fetchMedsData(action) {
     try {
-        const medsResponse = yield call(axios.get, 'http://localhost:8000/meds/' + action.payload.patientId);
+        const medsResponse = yield call(axios.get, 'https://birdiecare-api.herokuapp.com/meds/' + action.payload.patientId);
         yield put({ type: "DATA_MEDS_SUCCEEDED", meds: medsResponse.data.data });
     } catch (error) {
         yield put({ type: "DATA_MEDS_FAILED", message: error.message });
@@ -30,7 +30,7 @@ export function* fetchMedsData(action) {
 
 export function* fetchTasksData(action) {
     try {
-        const tasksResponse = yield call(axios.get, 'http://localhost:8000/checks/' + action.payload.patientId);
+        const tasksResponse = yield call(axios.get, 'https://birdiecare-api.herokuapp.com/checks/' + action.payload.patientId);
         yield put({ type: "DATA_TASKS_SUCCEEDED", tasks: tasksResponse.data.data });
     } catch (error) {
         yield put({ type: "DATA_TASKS_FAILED", message: error.message });
